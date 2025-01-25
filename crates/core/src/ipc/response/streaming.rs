be3ee21e255db34697f8afc7bd2f0863d94218e6
tonymushah <tonyramanantsalama@gmail.com>
2025-01-25 14:09:31 +0300

    Added `streaming` responses

---
+++ crates/core/src/ipc/response/streaming.rs
@@ -0,0 +1,42 @@
+use std::marker::PhantomData;
+
+use prost::Message;
+use tauri::{Emitter, EventTarget, Runtime, Webview};
+use tokio_stream::{Stream, StreamExt};
+
+use crate::ipc::{IpcMessage, IpcMessageBase};
+
+#[derive(Debug)]
+pub struct StreamingResponse<M, S> {
+    phantom: PhantomData<M>,
+    stream: S,
+}
+
+impl<M: Message, S: Stream<Item = Result<IpcMessage<M>, crate::Status>>> StreamingResponse<M, S> {
+    pub fn new(stream: S) -> Self {
+        Self {
+            phantom: PhantomData::<M>,
+            stream,
+        }
+    }
+    pub(crate) async fn send_responses<R: Runtime>(
+        self,
+        webview: Webview<R>,
+        event_stream_id: String,
+    ) -> tauri::Result<()> {
+        let mut stream = Box::pin(self.stream);
+        while let Some(res) = stream.next().await {
+            webview.emit_to(
+                EventTarget::webview(webview.label()),
+                &event_stream_id,
+                res.map(Into::<IpcMessageBase>::into),
+            )?;
+        }
+        webview.emit_to(
+            EventTarget::webview(webview.label()),
+            &event_stream_id,
+            None::<()>,
+        )?;
+        Ok(())
+    }
+}
