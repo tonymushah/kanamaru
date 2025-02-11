29199ca366b295c0ff1d904d1f85cfb2e72b174b
tonymushah <tonyramanantsalama@gmail.com>
2025-02-11 22:31:14 +0300

    added more codegen module

--- crates/core/src/codegen.rs
+++ crates/core/src/codegen.rs
@@ -1,2 +1,14 @@
-pub use tauri::{ipc::InvokeResolver, Runtime, Webview};
+pub use crate::{
+    ipc::{request::RawRequest, IpcMessageBase},
+    responder::{RPCType, Responder},
+    Status, StreamingRequest, StreamingResponse, UnaryRequest, UnaryResponse,
+};
+pub use async_trait::async_trait;
+pub use serde_json::to_value as to_json;
+pub use std::sync::Arc;
+pub use tauri::{
+    ipc::{InvokeError, InvokeResolver},
+    Runtime, Webview,
+};
+pub use tokio::{select, spawn};
 pub use tokio_stream;
